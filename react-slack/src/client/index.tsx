eaa88e27ba2e8ab79157bf060c1abb70b6162591
Pascal Fong Kye <pascal.fongkye@manomano.com>
2023-08-24 18:25:02 +0200

    feat(word-definition): call API to define input word

--- react-slack/src/client/index.tsx
+++ react-slack/src/client/index.tsx
@@ -1,35 +1,66 @@
 import { App } from "@slack/bolt";
 import SlackRenderer from "../lib/slackRenderer";
-import React from 'react';
+import React from "react";
 
 const app = new App({
-    token: process.env.SLACK_BOT_TOKEN,
-    signingSecret: process.env.SLACK_SIGNING_SECRET,
-    endpoints: '/api/v1/bender/events',
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  endpoints: "/api/v1/bender/events",
 });
 
-
 (async () => {
-    await app.start(process.env.PORT || 3000);
-    
-    console.log('Bolt app is running !!!');
+  await app.start(process.env.PORT || 3000);
+
+  console.log("Bolt app is running !!!");
 })();
 
 const reactSlack = new SlackRenderer(app);
+//server-side
 reactSlack.render(
-    <command value='/bender_stg_local' ack>
-        <modal title="This is my modal" onSubmit={async ({ack, view, client, body})=>{
-                        await ack();
-                        console.log(JSON.stringify(view))
-                        const {values: {my_prompt: {my_input:myInput}}} = view.state;
-                        await client.chat.postMessage({
-                            channel: body.user.id,
-                            text: `I am processing your request: ${myInput.value}`
-                        })
-                    }}>
-            <img title="Coffee time" url="https://coffee.alexflipnote.dev/random"/>
-            <section><text markdown>A message *with some bold text* and _some italicized text_.</text></section>
-            <input label="My Label" actionId="my_input" blockId="my_prompt" />
-        </modal>
-    </command>)
+  <command value="/react-slack" ack>
+    <modal
+      title="This is my modal"
+      //https://slack.dev/bolt-js/concepts#view-submissions
+      onSubmit={async ({ ack, view, client, body }) => {
+        await ack();
+        console.log(JSON.stringify(body));
+        const {
+          values: {
+            my_prompt: { my_input: myInput },
+          },
+        } = view.state;
+        await client.chat.postMessage({
+          channel: "C04TVQB89CG", //body.user.id,
+          text: `I am processing your request... :hourglass:`,
+        });
 
+        const definition = await defineWord(myInput.value);
+
+        await client.chat.postMessage({
+          channel: "C04TVQB89CG", //body.user.id,
+          text: `${myInput.value}: ${definition}`,
+        });
+      }}
+    >
+      <img title="Coffee time" url="https://coffee.alexflipnote.dev/random" />
+      <section>
+        <text markdown>
+          A message *with some bold text* and _some italicized text_.
+        </text>
+      </section>
+      <input label="My Label" actionId="my_input" blockId="my_prompt" />
+    </modal>
+  </command>
+);
+
+async function defineWord(word: string) {
+  try {
+    const response = await global.fetch(
+      "https://api.dictionaryapi.dev/api/v2/entries/en/" + word
+    );
+    const json = await response.json();
+    return json[0].meanings[0].definitions[0].definition;
+  } catch (error) {
+    return "Could not find a definition. Try another word or try again later.";
+  }
+}
--- react-slack/src/lib/slackRenderer.ts
+++ react-slack/src/lib/slackRenderer.ts
@@ -1,121 +1,118 @@
-import ReactReconciler from 'react-reconciler';
-import {App} from '@slack/bolt';
-
+import ReactReconciler from "react-reconciler";
+import { App } from "@slack/bolt";
 
 class SlackRenderer {
-    private readonly slackApp: App;
-    private readonly reactReconciler;
-    private rootContainer;
-    constructor(slackApp:App){
-        this.slackApp = slackApp;
-        this.reactReconciler = ReactReconciler(createHostConfig(slackApp));
-        this.rootContainer = this.reactReconciler.createContainer({}, false);
-    }
-    render(reactElement){
-        this.reactReconciler.updateContainer(reactElement, this.rootContainer);
-    }
+  private readonly slackApp: App;
+  private readonly reactReconciler;
+  private rootContainer;
+  constructor(slackApp: App) {
+    this.slackApp = slackApp;
+    this.reactReconciler = ReactReconciler(createHostConfig(slackApp));
+    this.rootContainer = this.reactReconciler.createContainer({}, false);
+  }
+  render(reactElement) {
+    this.reactReconciler.updateContainer(reactElement, this.rootContainer);
+  }
 }
 
 //the host environment configuration (for example DOM, mobile native platforms)
-function createHostConfig(slackApp:App){
-    return {
-        supportsMutation: true,
-        getRootHostContext(){
-
-        },
-        // This is where react-reconciler wants to create an instance of UI element in terms of the target.
-        createInstance(type, props){
-            if (type === 'command'){
-                if (typeof(props.children) === 'string'){
-                    slackApp.command(props.value, async ({ack, say, body})=> {
-                        if (props.ack){
-                            await ack();
-                        }
-                        await say(props.children);
-                    })
-                }
-                return {type, value: props.value, ack: props.ack};
-            } else if (type === 'modal'){
-                //TODO: find a way to better handle submit button (hard-code here) 
-                //-> either onSubmit on modal or a submit button for better customization (text + action)
-                //for now default is onSubmit handler on modal 
-                return {type, title:props.title, blocks:[], submit:{type:'plain_text', text:'hard_coded_submit'}, onSubmit:props.onSubmit};
-            } else if (type === 'text'){
-               const textElt:any = {text: props.children};
-               if (props.markdown) {
-                textElt.type = 'mrkdwn';
-               }
-               return textElt;
-            } else if (type === 'input'){
-                return {
-                    type,
-                    block_id: props.blockId,
-                    element: {
-                        type: 'plain_text_input',
-                        action_id: props.actionId || 'sl_input',
-                    },
-                    label: {
-                        type:'plain_text',
-                        text: props.label
-                    }
-                }
-            } else if (type === 'img') {
-                return {
-                    type: "image",
-                    title: {
-                        type: "plain_text",
-                        text: props.title
-                    },
-                    image_url: props.url,
-                    alt_text: props.alt || props.title
-                }
+function createHostConfig(slackApp: App) {
+  return {
+    supportsMutation: true,
+    getRootHostContext() {},
+    // This is where react-reconciler wants to create an instance of UI element in terms of the target.
+    createInstance(type, props) {
+      if (type === "command") {
+        if (typeof props.children === "string") {
+          slackApp.command(props.value, async ({ ack, say, body }) => {
+            if (props.ack) {
+              await ack();
             }
-            return {type};
-        },
-        createTextInstance(){},
-        prepareForCommit(){
-
-        },
-        resetAfterCommit(){
-
-        },
-        getChildHostContext(){},
-        shouldSetTextContent(){},
-        appendInitialChild(parent, child){
-            if (parent.type === 'command'){
-                if (child.type === 'modal'){
-                    const callback_id = 'view_1';
-                    slackApp.command(parent.value, async ({ack, body, client}) =>{
-                        if (parent.ack){
-                            await ack();
-                        }
-                        await client.views.open({
-                            trigger_id:body.trigger_id,
-                            view: {
-                                ...child,
-                                type:'modal',
-                                callback_id,
-                                title: {type:'plain_text', text: child.title}, 
-                            }
-                        })
-                    });
-                    
-                    slackApp.view(callback_id, child.onSubmit);
-                }
-            } else if (parent.type === 'modal') {
-                parent.blocks.push(child);
-            } else if (parent.type === 'section'){
-                if (child.type === 'mrkdwn'){
-                    parent.text = child;
-                }
+            await say(props.children);
+          });
+        }
+        return { type, value: props.value, ack: props.ack };
+      } else if (type === "modal") {
+        //TODO: find a way to better handle submit button (hard-code here)
+        //-> either onSubmit on modal or a submit button for better customization (text + action)
+        //for now default is onSubmit handler on modal
+        return {
+          type,
+          title: props.title,
+          blocks: [],
+          submit: { type: "plain_text", text: "hard_coded_submit" },
+          onSubmit: props.onSubmit,
+        };
+      } else if (type === "text") {
+        const textElt: any = { text: props.children };
+        if (props.markdown) {
+          textElt.type = "mrkdwn";
+        }
+        return textElt;
+      } else if (type === "input") {
+        return {
+          type,
+          block_id: props.blockId,
+          element: {
+            type: "plain_text_input",
+            action_id: props.actionId || "sl_input",
+          },
+          label: {
+            type: "plain_text",
+            text: props.label,
+          },
+        };
+      } else if (type === "img") {
+        return {
+          type: "image",
+          title: {
+            type: "plain_text",
+            text: props.title,
+          },
+          image_url: props.url,
+          alt_text: props.alt || props.title,
+        };
+      }
+      return { type };
+    },
+    createTextInstance() {},
+    prepareForCommit() {},
+    resetAfterCommit() {},
+    getChildHostContext() {},
+    shouldSetTextContent() {},
+    appendInitialChild(parent, child) {
+      if (parent.type === "command") {
+        if (child.type === "modal") {
+          const callback_id = "view_1";
+          slackApp.command(parent.value, async ({ ack, body, client }) => {
+            if (parent.ack) {
+              await ack();
             }
-        },
-        finalizeInitialChildren(){},
-        clearContainer(){},
-        appendChildToContainer(){}
-    }
+            await client.views.open({
+              trigger_id: body.trigger_id,
+              view: {
+                ...child,
+                type: "modal",
+                callback_id,
+                title: { type: "plain_text", text: child.title },
+              },
+            });
+          });
+
+          slackApp.view(callback_id, child.onSubmit);
+        }
+      } else if (parent.type === "modal") {
+        parent.blocks.push(child);
+      } else if (parent.type === "section") {
+        if (child.type === "mrkdwn") {
+          parent.text = child;
+        }
+      }
+    },
+    finalizeInitialChildren() {},
+    clearContainer() {},
+    appendChildToContainer() {},
+  };
 }
 
 export default SlackRenderer;
-
- 
